b122e449ab423e17df1806f8c145d56c934e5366
smonajemi <sina.monajemi@me.com>
2021-02-27 23:53:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -93,7 +93,9 @@ const fullname = (req.body.first_name + " " + req.body.last_name).toUpperCase();
 	    <h5>Thank you for reaching out! <br> I will get back to you soon.</h5>
         <h5>Best, <br> SINA MONAJEMI <br> <a href="https://smonajemi.com">smonajemi.com</a></h5>
         <hr>
-        <div style="text-align: left;text-transform:uppercase"><h5>Your message: <br>${req.body.message}</h5></div>
+        <div style="text-align: left;text-transform:uppercase"><h5>Your message: <br>${req.body.message}</h5>
+        <h7>Sent from ${req.body.email}</h7>
+        </div>
         `
     } 
 
