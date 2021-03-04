406d8d563c3b94cdf037b5f1dbee9c4cf3c38497
smonajemi <sina.monajemi@me.com>
2021-03-03 22:26:27 -0500

    comit

--- static/assets/js/mail.js
+++ static/assets/js/mail.js
@@ -10,37 +10,38 @@ function validation() {
     const phone = document.getElementById("phone").value;
     const message = document.getElementById("message").value;
     const data = fname || lname || email || phone || message;
-
     const error_message = document.getElementById("error_message");
     let exit = Boolean(true);
     var text;
+
+
     if(data.length == ''){
         text = "";
         error_message.innerHTML = text;
         return !exit;
     }else{
-    if(fname.length == ''){
-        text = "Please Enter Your First Name";
+    if(fname.length == '' && !isNaN(fname)){
+        text = "Please Enter Your First Name!";
         error_message.innerHTML = text.toUpperCase();
         return !exit;
     }
-    if(lname.length == ''){
-        text = "Please Enter Your Last Name";
+    if(lname.length == '' && !isNaN(lname)){
+        text = "Please Enter Your Last Name!";
         error_message.innerHTML = text.toUpperCase();
         return !exit;
     }
     if(email.length == ''){
-        text = "Please Enter Your Email Address";
+        text = "Please Enter a Valid Email Address!";
         error_message.innerHTML = text.toUpperCase();
         return !exit;
     }
-    if(phone.length < 10){
-        text = "Please Enter Your Phone Number";
+    if(phone.length < 10 && isNaN(phone)){
+        text = "Phone Number has to be atleast 10 digits!";
         error_message.innerHTML = text.toUpperCase();
         return !exit;
     }
     if(message.length < 5){
-        text = "Please Write a Message";
+        text = "Please Write Me a Short Message!";
         error_message.innerHTML = text.toUpperCase();
         return !exit;
     }
@@ -65,11 +66,51 @@ function validation() {
         }      
     return true;          
 }
-$(document).ready(function() {
+
+
+//   function emailvalidation()
+// {
+
+//     var input = document.form.email.value;
+
+//     var atsign = input.indexOf("@");
+//     var dotsign = input.lastIndexOf(".");
+
+//     while (input != null || input.length > 5)
+//     {
+//     if(atsign < 1 || dotsign < atsign + 2 || dotsign + 2 >= input.length)
+//     {
+//         alert("Email not valid");
+//         return false;
+//     }          
+//     else
+//     {
+//         return true;
+//     }  
+//     }        
+
+// }
+
+//   function isValid()
+//   {
+//       var flag = true;
+  
+//       flag &= emailvalidation();
+    
+  
+//       if (flag)
+//       {
+//           return true;
+//       }
+//       else
+//       {
+//           return false;
+//       }
+  
+//   } 
+
+  $(document).ready(function() {
     if(validation() == true){
         document.getElementById("myForm").reset();
       }
   });
-
-  
-
