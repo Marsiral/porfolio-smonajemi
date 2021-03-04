04f130f643700f4be2c490951fc7532a3ed8e801
smonajemi <sina.monajemi@me.com>
2021-03-04 01:09:58 -0500

    lock

--- static/assets/js/lock.js
+++ static/assets/js/lock.js
@@ -1,20 +1,56 @@
 document.addEventListener('contextmenu', function(e) {
     e.preventDefault();
   });
+
   document.onkeydown = function(e) {
-    if(event.keyCode == 123) {
+    if(e.key == 123) {
        return false;
     }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+    if(e.ctrlKey && e.shiftKey && e.key == 'I'.charCodeAt(0)) {
        return false;
     }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+    if(e.ctrlKey && e.shiftKey && e.key == 'C'.charCodeAt(0)) {
        return false;
     }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+    if(e.ctrlKey && e.shiftKey && e.key == 'J'.charCodeAt(0)) {
        return false;
     }
-    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+    if(e.ctrlKey && e.key == 'U'.charCodeAt(0)) {
        return false;
     }
   }
+
+  document.onkeypress  = function(e) {
+   if(e.key == 123) {
+      return false;
+   }
+   if(e.ctrlKey && e.shiftKey && e.key == 'I'.charCodeAt(0)) {
+      return false;
+   }
+   if(e.ctrlKey && e.shiftKey && e.key == 'C'.charCodeAt(0)) {
+      return false;
+   }
+   if(e.ctrlKey && e.shiftKey && e.key == 'J'.charCodeAt(0)) {
+      return false;
+   }
+   if(e.ctrlKey && e.key == 'U'.charCodeAt(0)) {
+      return false;
+   }
+ }
+   document.onmousedown = function(e) {
+      if(e.key == 123) {
+         return false;
+      }
+      if(e.ctrlKey && e.shiftKey && e.key == 'I'.charCodeAt(0)) {
+         return false;
+      }
+      if(e.ctrlKey && e.shiftKey && e.key == 'C'.charCodeAt(0)) {
+         return false;
+      }
+      if(e.ctrlKey && e.shiftKey && e.key == 'J'.charCodeAt(0)) {
+         return false;
+      }
+      if(e.ctrlKey && e.key == 'U'.charCodeAt(0)) {
+         return false;
+      }
+    }
