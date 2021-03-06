dc4135d12d4cf4aedea4e77b6f40105c4ad29382
smonajemi <sina.monajemi@me.com>
2021-03-06 03:51:05 -0500

    Update captcha.js

--- routes/captcha.js
+++ routes/captcha.js
@@ -1,7 +1,7 @@
 const e = require('express');
 const express = require('express');
 const router = express.Router();
-var Recaptcha = require('express-recaptcha').RecaptchaV2;
+var Recaptcha = require('express-recaptcha').RecaptchaV3;
 //import Recaptcha from 'express-recaptcha'
 var recaptcha = new Recaptcha('6LcO-HMaAAAAAOCceNR5rZt4rxHIp0F19c7mBdEA', '6LcO-HMaAAAAAKqjelidoiMWq2EoKzdbFjQyvyL4');
 //or with options
