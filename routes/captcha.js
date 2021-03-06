66d5afa2306bfd28a622c747dfaf9edf13ea7fd4
smonajemi <sina.monajemi@me.com>
2021-03-06 03:28:23 -0500

    Update captcha.js

--- routes/captcha.js
+++ routes/captcha.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 var Recaptcha = require('express-recaptcha').RecaptchaV3;
 //import Recaptcha from 'express-recaptcha'
-var recaptcha = new Recaptcha('SITE_KEY', 'SECRET_KEY');
+var recaptcha = new Recaptcha('6LcO-HMaAAAAAOCceNR5rZt4rxHIp0F19c7mBdEA', '6LcO-HMaAAAAAKqjelidoiMWq2EoKzdbFjQyvyL4');
 //or with options
 var options = {'hl':'de'};
-var recaptcha = new Recaptcha('6LcO-HMaAAAAAOCceNR5rZt4rxHIp0F19c7mBdEA', 'SECRET_KEY', options);
+var recaptcha = new Recaptcha('6LcO-HMaAAAAAOCceNR5rZt4rxHIp0F19c7mBdEA', '6LcO-HMaAAAAAKqjelidoiMWq2EoKzdbFjQyvyL4', options);
 
 router.post('/', recaptcha.middleware.verify, function(req, res){
   if (!req.recaptcha.error) {
