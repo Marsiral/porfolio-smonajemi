faa249919364538053e021a9668b85a360b4ca79
smonajemi <sina.monajemi@me.com>
2021-03-06 04:15:44 -0500

    Update captcha.js

--- routes/captcha.js
+++ routes/captcha.js
@@ -1,27 +1,32 @@
-const e = require('express');
 const express = require('express');
 const router = express.Router();
-
+var recaptcha = new Recaptcha('6LcO-HMaAAAAAOCceNR5rZt4rxHIp0F19c7mBdEA', '6LcO-HMaAAAAAKqjelidoiMWq2EoKzdbFjQyvyL4');
 router.post('/',function(req,res){
-  
-  if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null || req.body === null) {
-    return res.render("partials/contact.hbs", { errorMsg: "NOOO", title: 'Contact Me'});        
-  } else{
-    res.redirect('/thankyouPage');  
-  }
+  recaptcha.verify(req, function(error, data){
+  // if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null || req.body === null) {
+  //   return res.render("partials/contact.hbs", { errorMsg: "NOOO", title: 'Contact Me'});        
+  // } else{
+  //   res.redirect('/thankyouPage');  
+  // }
 
-  var secretKey = "6LcO-HMaAAAAAKqjelidoiMWq2EoKzdbFjQyvyL4";
-
-  var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
-  // Hitting GET request to the URL, Google will respond with success or error scenario.
-  request(verificationUrl,function(error,response,body) {
-    body = JSON.parse(body);
-    // Success will be true or false depending upon captcha validation.
-    if(body.success !== undefined && !body.success) {
-      return res.json({"responseCode" : 1,"responseDesc" : "Failed captcha verification"});
+    if (!req.recaptcha.error) {
+      res.redirect('/thankyouPage');  
+    } else {
+      return res.render("partials/contact.hbs", { errorMsg: "NOOO", title: 'Contact Me'}); 
     }
-    return true;
   });
+
+
+  // var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+  // Hitting GET request to the URL, Google will respond with success or error scenario.
+  // request(verificationUrl,function(error,response,body) {
+  //   body = JSON.parse(body);
+  //   // Success will be true or false depending upon captcha validation.
+  //   if(body.success !== undefined && !body.success) {
+  //     return res.json({"responseCode" : 1,"responseDesc" : "Failed captcha verification"});
+  //   }
+  //   return true;
+  // });
 });
 
 module.exports = router;
