97b3e6669e8eb3182db1c5754af2f6aff357f747
smonajemi <sina.monajemi@me.com>
2021-03-06 04:22:31 -0500

    Update captcha.js

--- routes/captcha.js
+++ routes/captcha.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-var recaptcha = new Recaptcha('6LcO-HMaAAAAAOCceNR5rZt4rxHIp0F19c7mBdEA', '6LcO-HMaAAAAAKqjelidoiMWq2EoKzdbFjQyvyL4');
+
 router.post('/',function(req,res){
   
   if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null || req.body === null) {
@@ -8,19 +8,19 @@ router.post('/',function(req,res){
   } else{
     res.redirect('/thankyouPage');  
   }
-  
 
+  var secretKey = "6LcO-HMaAAAAAKqjelidoiMWq2EoKzdbFjQyvyL4";
 
-  // var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+  var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
   // Hitting GET request to the URL, Google will respond with success or error scenario.
-  // request(verificationUrl,function(error,response,body) {
-  //   body = JSON.parse(body);
-  //   // Success will be true or false depending upon captcha validation.
-  //   if(body.success !== undefined && !body.success) {
-  //     return res.json({"responseCode" : 1,"responseDesc" : "Failed captcha verification"});
-  //   }
-  //   return true;
-  // });
+  request(verificationUrl,function(error,response,body) {
+    body = JSON.parse(body);
+    // Success will be true or false depending upon captcha validation.
+    if(body.success !== undefined && !body.success) {
+      return res.json({"responseCode" : 1,"responseDesc" : "Failed captcha verification"});
+    }
+    return true;
+  });
 });
 
 module.exports = router;
