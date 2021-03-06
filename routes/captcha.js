2fe4e867e41f118d42c23f20f66eb50b26372f45
smonajemi <sina.monajemi@me.com>
2021-03-06 03:27:49 -0500

    Update captcha.js

--- routes/captcha.js
+++ routes/captcha.js
@@ -5,11 +5,8 @@ var Recaptcha = require('express-recaptcha').RecaptchaV3;
 var recaptcha = new Recaptcha('SITE_KEY', 'SECRET_KEY');
 //or with options
 var options = {'hl':'de'};
-var recaptcha = new Recaptcha('6LcO-HMaAAAAAOCceNR5rZt4rxHIp0F19c7mBdEA', '6LcO-HMaAAAAAKqjelidoiMWq2EoKzdbFjQyvyL4', options);
+var recaptcha = new Recaptcha('6LcO-HMaAAAAAOCceNR5rZt4rxHIp0F19c7mBdEA', 'SECRET_KEY', options);
 
-router.get('/', recaptcha.middleware.renderWith({'hl':'fr'}), function(req, res){
-  res.render('index', { captcha:res.recaptcha });
-});
 router.post('/', recaptcha.middleware.verify, function(req, res){
   if (!req.recaptcha.error) {
     // success code
