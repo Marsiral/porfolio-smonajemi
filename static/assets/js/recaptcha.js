7fea6ace5a69f98e57df89958877b4e166a2afc7
smonajemi <sina.monajemi@me.com>
2021-03-07 02:04:22 -0500

    sad

---
+++ static/assets/js/recaptcha.js
@@ -0,0 +1,19 @@
+
+function myFunction() {
+    var res = grecaptcha.getResponse();
+    var flag = false;
+    if(res.length == 0) {
+        document.getElementById('captcha').innerHTML="You can't leave Captcha Code empty";
+        flag = false;
+    } else {
+        document.getElementById("captcha").typ = 'submit';
+         flag = true;
+    }
+    return flag;
+  }
+
+//   <div class="g-recaptcha" id="rcaptcha" data-sitekey="6Lcj6XQaAAAAAI0s9ja70-abMFJy-LCUOjP2S5uh"></div>
+//   <span id="captcha" style="margin-left:100px;color:red" />
+//   <button onclick="myFunction()">Send</button>
+//   <br><hr>
+//   <input type="" id="submit" class="btn"></input>
