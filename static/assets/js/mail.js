2153c1981265d575ad74f8b857839c7fe73c7ca3
smonajemi <sina.monajemi@me.com>
2021-03-07 02:26:53 -0500

    Update mail.js

--- static/assets/js/mail.js
+++ static/assets/js/mail.js
@@ -13,7 +13,7 @@ function validation() {
 
   
 
-    if(data.length == ''){
+    if(data.length == '' && res.length == 0){
         text = "";
         error_message.innerHTML = text;
         return !exit;
