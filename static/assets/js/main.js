746c639de1302f6dbae9722bed441da1e099efc5
smonajemi <sina.monajemi@me.com>
2021-03-21 22:05:22 -0400

    haji

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -194,3 +194,14 @@
   });
 
 })(jQuery);
+
+
+
+
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
